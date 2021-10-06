58595da78fe753b0a97f2309cf69cae813b4d9e1
Valeria Tyan <vlrtyan@yandex.ru>
2021-10-06 23:52:53 +0300

    feat: popup

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const popupCloseButton = document.querySelector('.popup__close-button');
+const form = document.querySelector('.popup__form');
+const nameField = document.querySelector('.popup__input_name');
+const nameProfile = document.querySelector('.profile__name');
+const descriptionField = document.querySelector('.popup__input_description');
+const descriptionProfile = document.querySelector('.profile__description');
+
+function openPopup(){
+    popup.classList.add('popup_opened')
+}
+
+function closePopup(){
+    popup.classList.remove('popup_opened')
+}
+
+function sumbitForm(event){
+    event.preventDefault();
+    nameProfile.textContent = nameField.value;
+    descriptionProfile.textContent = descriptionField.value;
+    closePopup();
+
+}
+
+editButton.addEventListener('click', openPopup)
+
+popupCloseButton.addEventListener('click', closePopup)
+
+form.addEventListener('submit', sumbitForm)
+
+
