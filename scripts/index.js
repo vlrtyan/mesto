33650cf7c216477e4296715d66b0cd6027cb18b7
Valeria Tyan <vlrtyan@yandex.ru>
2021-10-25 23:53:47 +0300

    feat: добавить попап для add button

--- scripts/index.js
+++ scripts/index.js
@@ -32,3 +32,62 @@ popupCloseButton.addEventListener('click', closePopup)
 form.addEventListener('submit', sumbitForm)
 
 
+const newItem = document.querySelector('.new-item');
+const addButton = document.querySelector('.profile__add-button')
+const newItemFormCloseButton = document.querySelector('.new-item__close-button');
+const newItemForm = document.querySelector('.new-item__form');
+const placeNameField = document.querySelector('.new-item__input_type_name');
+const placeNameCard = document.querySelector('.element__title');
+const imageField = document.querySelector('.new-item__input_type_link');
+const imageCard = document.querySelector('.element__image');
+
+function openNewItem(){
+    placeNameField.value = placeNameCard.textContent;
+    imageField.value = imageCard.textContent;
+    newItem.classList.add('new-item_opened');
+}
+
+function closeNewItem(){
+    newItem.classList.remove('new-item_opened')
+}
+
+function sumbitNewItemForm(event){
+    event.preventDefault();
+    placeNameCard.textContent = placeNameField.value;
+    imageCard.textContent = imageField.value;
+    closePopup();
+
+}
+
+addButton.addEventListener('click', openNewItem)
+
+newItemFormCloseButton.addEventListener('click', closeNewItem)
+
+newItemForm.addEventListener('submit', sumbitNewItemForm)
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
