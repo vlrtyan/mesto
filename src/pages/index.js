2657ac7056bd108722a7711418c8abebf12ef4fb
Valeria Tyan <vlrtyan@yandex.ru>
2022-01-05 22:52:00 +0300

    feat: добавить удаление только своих карточек

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Section from '../scripts/Section.js';
 import PopupWithImage from '../scripts/PopupWithImage.js';
 import PopupWithForm from '../scripts/PopupWithForm.js';
 import UserInfo from '../scripts/UserInfo.js';
-import {config, editButton, nameField, nameProfile, descriptionField, descriptionProfile, addButton, placeNameField, imageField, cardsListSection, cardTemplateSelector} from '../utils/constants.js';
+import { config, editButton, nameField, nameProfile, descriptionField, descriptionProfile, addButton, placeNameField, imageField, cardsListSection, cardTemplateSelector } from '../utils/constants.js';
 import Api from '../scripts/Api.js';
 
 
@@ -16,24 +16,22 @@ const api = new Api({
 })
 
 //отрисовка карточек с сервера
-function renderInitialCards(){
-Promise.all([api.getUserData(), api.getInitialCards()])
-.then(([userData, initialCards]) => {
-  console.log(userData, initialCards);
-  const user = userData;
-  const cardsList = new Section({
-    items: initialCards,
-    renderer: item => {
-      //console.log(item.likes.length);
-      //console.log(item.querySelector('.element__like-counter').value);
-      cardsList.addItem(createCard(item));
-    },
-  }, cardsListSection);
-  cardsList.renderItems();
-})
-.catch(err => {
-  console.log(err)
-})
+function renderInitialCards() {
+  Promise.all([api.getUserData(), api.getInitialCards()])
+    .then(([userData, initialCards]) => {
+      console.log(userData, initialCards);
+      userID = userData._id;
+      const cardsList = new Section({
+        items: initialCards,
+        renderer: item => {
+          cardsList.addItem(createCard(item));
+        },
+      }, cardsListSection);
+      cardsList.renderItems();
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
 
 renderInitialCards();
@@ -41,13 +39,14 @@ renderInitialCards();
 const popupWithImage = new PopupWithImage('.image-popup');
 
 //создание карточки
-function createCard(item){ 
+function createCard(item) {
   const card = new Card(item, cardTemplateSelector, () => {
-    popupWithImage.open(item.name, item.link);
-  });
-  const cardElement = card.generateCard(); 
+      popupWithImage.open(item.name, item.link);
+    }, userID);
+  const cardElement = card.generateCard();
   return cardElement;
 }
+
 
 //валидация форм
 const formNameValidator = new FormValidator(document.querySelector('.name-popup__form'), config);
@@ -64,7 +63,7 @@ const newItemPopup = new PopupWithForm('.new-item', addCard);
 const userInfo = new UserInfo({ userNameSelector: nameProfile, profileDescriptionSelector: descriptionProfile });
 
 //редактирование профиля
-function editProfile(newUserInfo){
+function editProfile(newUserInfo) {
   userInfo.setUserInfo(newUserInfo);
   namePopup.close();
 
@@ -72,50 +71,53 @@ function editProfile(newUserInfo){
     name: userInfo.getUserInfo().name,
     about: userInfo.getUserInfo().description
   })
-  .then(res => {
-    console.log(res)
-  })
-  .catch(err => console.log(`Ошибка при редактировании профиля: ${err}`));
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => console.log(`Ошибка при редактировании профиля: ${err}`));
 }
 
 //добавление новой карточки
-function addCard(){
+function addCard() {
   const name = placeNameField.value;
   const link = imageField.value;
   const item = {
     name, link
   }
   Promise.all([api.getUserData(), api.addNewItem(item)])
-  .then(([userData, newItem]) => {
-    console.log(newItem);
-    const user = userData;
-    const cardsList = new Section ({
-      items: newItem,
-      renderer: (item) => {
-        cardsList.addItem(createCard(item));
-      },
-    }, cardsListSection);
-    cardsList.renderItems();
-    newItemPopup.close();
-  })
-  .catch(err => console.log(`Ошибка при добавлении новой карточки: ${err}`))
-  }
+    .then(([userData, newItem]) => {
+      console.log(newItem);
+      const user = userData;
+      const cardsList = new Section({
+        items: newItem,
+        renderer: (item) => {
+          cardsList.addItem(createCard(item));
+        },
+      }, cardsListSection);
+      cardsList.renderItems();
+      newItemPopup.close();
+    })
+    .catch(err => console.log(`Ошибка при добавлении новой карточки: ${err}`))
+}
+
+//подтверждение удаления карточки
+//function confirmDelete(){}
 
 window.addEventListener('load', () => {
-    document.querySelectorAll('.popup').forEach((popup) => popup.classList.add('popup__transition'));
-  })
+  document.querySelectorAll('.popup').forEach((popup) => popup.classList.add('popup__transition'));
+})
 
 editButton.addEventListener('click', () => {
-    formNameValidator.resetForm();
-    const userData = userInfo.getUserInfo();
-    nameField.value = userData.name;
-    descriptionField.value = userData.description;
-    namePopup.open();
+  formNameValidator.resetForm();
+  const userData = userInfo.getUserInfo();
+  nameField.value = userData.name;
+  descriptionField.value = userData.description;
+  namePopup.open();
 })
 
 addButton.addEventListener('click', () => {
-    formNewItemValidator.resetForm();
-    placeNameField.value = '';
-    imageField.value = '';
-    newItemPopup.open();
+  formNewItemValidator.resetForm();
+  placeNameField.value = '';
+  imageField.value = '';
+  newItemPopup.open();
 })
--- src/scripts/Card.js
+++ src/scripts/Card.js
@@ -1,12 +1,16 @@
 export default class Card {
-    constructor(data, templateSelector, handleCardClick){
+    constructor(data, templateSelector, handleCardClick, userID){
         this._handleCardClick = handleCardClick;
+        //this._handleDeleteCard = handleDeleteCard;
         this._templateSelector = templateSelector;
         this._name = data.name;
         this._link = data.link;
         this._likeCouner = data.likes;
         this._delete = this._delete.bind(this);
         this._like = this._like.bind(this);
+        this._handleDeleteButtonVisibility = this._handleDeleteButtonVisibility.bind(this);
+        this._userID = userID;
+        this._cardOwner = data.owner._id;
     }
 
     _getTemplate() {
@@ -32,6 +36,17 @@ export default class Card {
         this._image.addEventListener('click', () => {
             this._handleCardClick(this._name, this._link)});
     }
+
+    _handleDeleteButtonVisibility(){
+        this._deleteButton = this._element.querySelector('.element__delete-button');
+        if (this._userID === this._cardOwner) {
+            console.log('Моя карта');
+        } else {
+            this._deleteButton.remove();
+            this._deleteButton.removeEventListener('click', this._delete);
+            console.log ('Не моя карта');
+        }
+    }
     
     generateCard(){
         this._element = this._getTemplate();
@@ -41,6 +56,8 @@ export default class Card {
         this._image.setAttribute('src', this._link);
         this._image.setAttribute('alt', this._name);
         this._element.querySelector('.element__like-counter').innerText = this._likeCouner.length;
+        this._handleDeleteButtonVisibility();
+        //console.log(this._userID, this._cardOwner)
         
         return this._element;
     }
